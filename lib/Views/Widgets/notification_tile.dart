ece2cd234a6cf7aaf043aaa465caa9bcb3f84189
Karim Ashraf <karimashraf354@gmail.com>
2024-12-17 19:04:31 +0200

    nothing

--- lib/Views/Widgets/notification_tile.dart
+++ lib/Views/Widgets/notification_tile.dart
@@ -92,16 +92,3 @@ class NotificationTile extends StatelessWidget {
     );
   }
 }
-class MyWidget extends StatefulWidget {
-  const MyWidget({super.key});
-
-  @override
-  State<MyWidget> createState() => _MyWidgetState();
-}
-
-class _MyWidgetState extends State<MyWidget> {
-  @override
-  Widget build(BuildContext context) {
-    return const Placeholder();
-  }
-}
